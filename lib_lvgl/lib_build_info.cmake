ac7f8e754a74685f1499a4f29d7450645ba3e160
LeslieXMOS <leslielee@xmos.com>
2025-05-19 10:24:48 +0800

    Init library

---
+++ lib_lvgl/lib_build_info.cmake
@@ -0,0 +1,21 @@
+set(LIB_NAME lib_lvgl)
+set(LIB_VERSION 9.2.0)
+
+set(LVGL_PATH       ${CMAKE_CURRENT_LIST_DIR}/submodules/lvgl)
+set(LIB_INCLUDES    submodules/lvgl)
+
+file(GLOB_RECURSE LIB_C_SRCS RELATIVE ${CMAKE_CURRENT_LIST_DIR} "${LVGL_PATH}/src/*.c" "${LVGL_PATH}/demos/*.c" "${LVGL_PATH}/examples/*.c")
+file(GLOB_RECURSE LIB_CXX_SRCS RELATIVE ${CMAKE_CURRENT_LIST_DIR} "${LVGL_PATH}/src/*.cpp")
+
+set(LIB_COMPILER_FLAGS)
+
+execute_process(
+    COMMAND git reset --hard
+    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/submodules/lvgl
+)
+execute_process(
+    COMMAND git apply ../lvgl_xmos.patch
+    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/submodules/lvgl
+)
+
+XMOS_REGISTER_MODULE()
